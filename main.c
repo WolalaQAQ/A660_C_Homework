65bc51ff54b57cf31814bef090263eed9fa3615e
Wolala <xgw20040129@outlook.com>
2023-03-21 00:04:18 +0800

    换GTK3

--- main.c
+++ main.c
@@ -1,41 +1,37 @@
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-            gpointer   data)
-{
-  g_print ("Hello World\n");
+static void on_button_clicked(GtkWidget *widget, gpointer data) {
+  g_print("Hello, GTK3!\n");
 }
 
-static void
-activate (GtkApplication *app,
-         gpointer        user_data)
-{
+static void on_activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
   GtkWidget *button;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  // 创建窗口
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "GTK3 Example");
+  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
 
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  gtk_window_set_child (GTK_WINDOW (window), button);
+  // 创建按钮并设置标签
+  button = gtk_button_new_with_label("Click me!");
+  g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
 
-  gtk_window_present (GTK_WINDOW (window));
+  // 将按钮添加到窗口中
+  gtk_container_add(GTK_CONTAINER(window), button);
+
+  // 显示窗口及其所有子部件
+  gtk_widget_show_all(window);
 }
 
-int
-main (int    argc,
-     char **argv)
-{
+int main(int argc, char *argv[]) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+  app = gtk_application_new("com.example.gtk3", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
 
   return status;
 }
---
+++ ml/YoloFaceDetector.c
@@ -0,0 +1,4 @@
+//
+// Created by wolala on 2023/3/20.
+//
+#include "YoloFaceDetector.h"
